5a5bb3ce0abacccb8d1f5169426197ba17a1295b
M <33653026+climbthewall@users.noreply.github.com>
2020-03-25 01:15:46 +0530

    Update helpline.js

--- src/components/helpline.js
+++ src/components/helpline.js
@@ -5,7 +5,7 @@ const HelpLine = () => {
        <div className="HelpLine">
 
       <div className="link2 fadeInUp" style={{animationDelay: '0.2s'}}>
-        <h3>Helpline Numbers: Kerala</h3><
+        <h3>Helpline Numbers: Kerala</h3><br></br>
         <img src="https://raw.githubusercontent.com/mighildotcom/covid19-kerala-gov-img-library/master/kk-shailaja-disha.jpg" class="responsive" alt="DHS Kerala Poster"/>
         <img src="https://raw.githubusercontent.com/mighildotcom/covid19-kerala-gov-img-library/master/covid19%20call%20centre.jpg" class="responsive" alt="DHS Kerala Poster"/>
         <img src="https://raw.githubusercontent.com/mighildotcom/covid19-kerala-gov-img-library/master/psychological-help.jpeg" class="responsive" alt="DHS Kerala Poster"/>     
